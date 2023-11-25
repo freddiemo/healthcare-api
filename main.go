29e3a08f21e4d02c66932a86c026acc09a27cf62
Freddie Molina <freddiejmo@gmail.com>
2023-11-25 15:28:36 -0300

    setup api server

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-
+	"github.com/freddiemo/healthcare-api/api"
 	"github.com/freddiemo/healthcare-api/config"
 	"github.com/freddiemo/healthcare-api/db"
 )
@@ -10,13 +9,5 @@ import (
 func main() {
 	params := config.Init()
 	db.Init(params)
-	server := gin.Default()
-
-	server.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "OK!",
-		})
-	})
-
-	server.Run(":" + params["APP_PORT"])
+	api.Init(params)
 }
