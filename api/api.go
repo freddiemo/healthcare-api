048d7bc9547cc71a2d1a927d0c8e6a0c303897e5
Freddie Molina <freddiejmo@gmail.com>
2023-11-30 09:12:56 -0300

    added api_integration to docker

--- api/api.go
+++ api/api.go
@@ -23,26 +23,26 @@ func NewHealthcareAPI(
 // @title        Healthcare API
 // @version      1.0
 // @description  A Healthcare service API in Go using Gin
-// @BasePath  /v1
+// @BasePath     /v1
 
 // ListDiagnostics godoc
 // @Summary      List diagnostics
 // @Description  List dignostic data from bd.
 // @Tags         diagnostics
 // @Produce      application/json
-// @Param       firstName query string false "Filter diagnostics by firstName"
-// @Param       lastName query string false "Filter diagnostics by lastName"
-// @Param       date query string false "Filter diagnostics by date"
-// @Success      200  {array}  model.DiagnosticWithPatient{}
+// @Param        firstName  query    string  false  "Filter diagnostics by firstName"
+// @Param        lastName   query    string  false  "Filter diagnostics by lastName"
+// @Param        date       query    string  false  "Filter diagnostics by date"
+// @Success      200        {array}  model.DiagnosticWithPatient{}
 // @Router       /diagnostics [get]
-func (api *HealthcareAPI) SaveDiagnostic(ctx *gin.Context) {
-	diagnostic, err := api.diagnosticsController.Save(ctx)
+func (api *HealthcareAPI) FindDiagnostics(ctx *gin.Context) {
+	diagnostics, err := api.diagnosticsController.Find(ctx)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, &Response{
 			Message: err.Error(),
 		})
 	} else {
-		ctx.JSON(http.StatusCreated, diagnostic)
+		ctx.JSON(http.StatusOK, diagnostics)
 	}
 }
 
@@ -55,13 +55,13 @@ func (api *HealthcareAPI) SaveDiagnostic(ctx *gin.Context) {
 // @Success      201  {object}  model.Diagnostic{}
 // @Failure      400  {object}  Response{}
 // @Router       /diagnostics [post]
-func (api *HealthcareAPI) FindDiagnostics(ctx *gin.Context) {
-	diagnostics, err := api.diagnosticsController.Find(ctx)
+func (api *HealthcareAPI) SaveDiagnostic(ctx *gin.Context) {
+	diagnostic, err := api.diagnosticsController.Save(ctx)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, &Response{
 			Message: err.Error(),
 		})
 	} else {
-		ctx.JSON(http.StatusOK, diagnostics)
+		ctx.JSON(http.StatusCreated, diagnostic)
 	}
 }
